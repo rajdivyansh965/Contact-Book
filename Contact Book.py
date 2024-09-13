540208ae2458fd7e16a6ed21d492afeece6f2bd3
rajdivyansh965 <137403837+rajdivyansh965@users.noreply.github.com>
2024-09-13 16:38:34 +0530

    Contact Book.py

--- Contact Book.py
+++ Contact Book.py
@@ -10,8 +10,8 @@ width = 1200
 height = 600
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-x = (screen_width / 2) - (width / 2)
-y = (screen_height / 2) - (height / 2)
+x = (screen_width) - (width)
+y = (screen_height) - (height)
 root.geometry("%dx%d+%d+%d" % (width, height, x, y))
 root.resizable(0, 0)
 root.config(bg="#6666ff")
